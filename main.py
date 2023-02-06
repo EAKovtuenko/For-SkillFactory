bd5dbc22840b5aaffa7a2dcc280755a5bb3b3986
EAKovtuenko <3884985@bk.ru>
2023-02-06 18:15:11 +0300

    Task 13.8.19

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = int(input("Введите сумму: "))
-deposit = []
-for rate in per_cent.values():
-    deposit.append(int(rate*money/100))
-max_deposit = max(deposit)
-print('Максимальная сумма, которую вы можете заработать —', str(max_deposit))
+tikets = int(input("Больше 4 билетов - скидка 10% \n"
+                   "Введите количество билетов: "))
+age = list(map(int, input("Укажите возраст посетителей: ").split()))
+
+while tikets != len(age):
+    age = list(map(int, input("Количество поситетелей не совпадает с количеством билетов. \n"
+                              "Укажите возраст поситетелей: ").split()))
+price = []
+for i in age:
+    if i in range (0, 18):
+        price.append(0)
+    elif i in range (18, 25):
+        price.append(990)
+    else:
+        price.append(1390)
+if tikets > 3:
+    print("Сумма со скидкой: ", sum(price) - ((sum(price) / 100)* 10),"рублей")
+else:
+    print("Сумма: ", sum(price), "рублей")
